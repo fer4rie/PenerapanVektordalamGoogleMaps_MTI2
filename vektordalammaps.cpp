ed398a822b82b3cf856852324b23d8750f808392
fer4rie <firapuspita2106@gmail.com>
2026-06-14 13:51:34 +0700

    implementation jarakEuclidean function

--- vektordalammaps.cpp
+++ vektordalammaps.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+// Rumus Euclidean: √((x2 - x1)^2 + (y2 - y1)^2)
+double jarakEuclidean(double x1, double y1, double x2, double y2) {
+    return sqrt(
+        pow((x2 - x1), 2) +
+        pow((y2 - y1), 2)
+    ) * 111; // dikalikan 111 karena 1° lintang ≈ 111 km (konversi dari derajat ke kilometer)
+}
